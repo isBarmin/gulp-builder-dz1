aceb83eec0ca3141545ceef7602314f76c9040a6
ilya barmin <isBarmin@yandex.ru>
2016-09-25 00:10:16 +0500

    change task "watch"

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -5,8 +5,8 @@ module.exports = function() {
     $.gulp.watch('./source/js/**/*.js', $.gulp.series('js:process'));
     $.gulp.watch('./source/style/**/*.scss', $.gulp.series('sass'));
     $.gulp.watch('./source/template/**/*.pug', $.gulp.series('pug'));
-    $.gulp.watch('./source/images/**/*.*', $.gulp.series('copy:image'));
+    $.gulp.watch('./source/images/*.*', $.gulp.series('copy:image'));
     $.gulp.watch('./source/fonts/**/*.*', $.gulp.series('copy:fonts'));
-    // $.gulp.watch('./source/*.*', $.gulp.series('copy:assets'));
+    $.gulp.watch('./source/*.*', $.gulp.series('copy:assets'));
   });
 };
